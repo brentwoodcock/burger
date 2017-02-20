439fc8e4b53d27b1681a4c052cd455a718f97a69
Brent Woodcock <brentwoodcock@gmail.com>
2017-02-20 14:13:23 -0600

    modified connection to deploy on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'bwoodcock',
-	password: '',
-	database: 'burgers_db'
-});
+var connection;
+
+// Configure connection for Heroku
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {  // Configure connection for localhost
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'bwoodcock',
+		password: '',
+		database: 'burgers_db'
+	});
+}
 
 connection.connect(function(err) {
 	if (err) {
